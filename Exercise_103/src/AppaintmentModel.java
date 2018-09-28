620c3ccc8c16c6e56438f4380822e3edd5857ed2
Michael Kahr <mikey.kahr@gmail.com>
2018-09-28 13:02:48 +0200

    created all classes

---
+++ Exercise_103/src/AppaintmentModel.java
@@ -0,0 +1,29 @@
+
+import java.util.LinkedList;
+import javax.swing.AbstractListModel;
+
+public class AppaintmentModel extends AbstractListModel{
+
+    private LinkedList<Appointment> appointments = new LinkedList<>();
+    
+    public void add(Appointment t){
+        appointments.add(t);
+        fireIntervalAdded(this, 0, appointments.size()-1);
+    }
+    
+    public void remove(Appointment t){
+        appointments.remove(t);
+        fireContentsChanged(this, 0, appointments.size()-1);
+    }
+    
+    @Override
+    public int getSize() {
+        return appointments.size();
+    }
+
+    @Override
+    public Object getElementAt(int index) {
+        return appointments.get(index);
+    }
+
+}
